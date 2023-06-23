29059051878c1831f7525ff9860c75166d3478d4
İhsan Doğan <ihsannote3@gmail.com>
2023-06-24 01:27:45 +0300

    yeni1

--- client/src/Filmler/Film.js
+++ client/src/Filmler/Film.js
@@ -1,25 +1,27 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 
-export default function Film(props) {
+export default function Film({ KaydedilenlerListesineEkle }) {
   const [movie, setMovie] = useState();
 
-  let id = 1;
+  let { id } = useParams();
   // URL'den alınan :id parametresini bu değişkene aktarın
 
   useEffect(() => {
     axios
       .get(`http://localhost:5001/api/filmler/${id}`) // Bu uç noktayı Postman'le çalışın
-      .then(response => {
-          // Bu kısmı log statementlarıyla çalışın
-          // ve burdan gelen response'u 'movie' e aktarın
+      .then((response) => {
+        setMovie(response.data);
+        // Bu kısmı log statementlarıyla çalışın
+        // ve burdan gelen response'u 'movie' e aktarın
       })
-      .catch(error => {
+      .catch((error) => {
         console.error(error);
       });
     // Bu effect her `id ` değiştiğinde çalışmalı
     // Bunu nasıl gerçekleştirebiliriz?
-  }, []);
+  }, [id]);
 
   // Yalnızca esnek görevlere geçtiğinizde burdaki yorum etiketini kaldırın
   // const filmiKaydet = evt => { }
@@ -42,13 +44,18 @@ export default function Film(props) {
         </div>
         <h3>Actors</h3>
 
-        {stars.map(star => (
+        {stars.map((star) => (
           <div key={star} className="movie-star">
             {star}
           </div>
         ))}
       </div>
-      <div className="save-button">Kaydet</div>
+      <div
+        onClick={() => KaydedilenlerListesineEkle(id)}
+        className="save-button"
+      >
+        Kaydet
+      </div>
     </div>
   );
 }
--- client/src/Filmler/KaydedilenlerListesi.js
+++ client/src/Filmler/KaydedilenlerListesi.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React from "react";
+import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
 
 export default function KaydedilenlerListesi(props) {
   return (
     <div className="saved-list">
       <h3>Kaydedilen Filmler:</h3>
-      {props.list.map(movie => (
-        <span className="saved-movie">{movie.title}</span>
+      {props.list.map((movie) => (
+        <span className="saved-movie">
+          {props.movies.find((a) => a.id == movie).title}
+        </span>
       ))}
-      <div className="home-button">Anasayfa</div>
+      <NavLink to="/">
+        <div className="home-button">Anasayfa</div>
+      </NavLink>
     </div>
   );
 }
